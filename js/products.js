fd54f0fb227836cb8d2cf7facfbe2eaeb0e948e6
arielRas <ariel.ras.90@gmail.com>
2025-07-07 16:04:01 -0300

    Se crea funcion productCardEventSuscribe para suscribir la tarjeta de producto y sus botones a eventos

--- js/products.js
+++ js/products.js
@@ -1,6 +1,35 @@
-let cart = [];
+/* -------------------------------------------------------------------------- */
+/*                                  FUNCIONES                                 */
+/* -------------------------------------------------------------------------- */
+async function getAllProducts(limit, skip) {
 
-function ToViewModel({ id, title, brand, price, images, description }) {
+    try {
+        const response = await fetch(`https://dummyjson.com/products/category/smartphones?limit=${limit}&skip=${skip}`)
+
+        if (!response.ok)
+            throw new Error(`Error al intentar obtener los productos - Status Code: ${response.status}`);
+
+        const data = await response.json();
+        
+        return data.products.map(x => toViewModel(x));
+    }
+    catch (error) {
+        console.error(error.message);
+        return null;
+    }
+}
+
+async function renderProducts(numberPage) {
+    const products = await getAllProducts(12, numberPage);
+
+    const productsGrid = document.querySelector('.grid-products');
+
+    products.forEach(product => {
+        productsGrid.appendChild(createProductCard(product))
+    });
+}
+
+function toViewModel({ id, title, brand, price, images, description }) {
     return {
         id,
         title,
@@ -11,9 +40,24 @@ function ToViewModel({ id, title, brand, price, images, description }) {
     };
 }
 
+function productCardEventSuscribe(productCard){ 
+    const quantity = productCard.querySelector('.kart-options-container input');  
+    
+    productCard.querySelector('.min-btn').addEventListener('click', () =>{
+        if(Number(quantity.value) > 1)
+            quantity.value = Number(quantity.value) - 1;
+    });
+
+    productCard.querySelector('.max-btn').addEventListener('click', () =>{
+        if(Number(quantity.value) < 10)
+            quantity.value = Number(quantity.value) + 1;
+    });
+}
+
 function createProductCard(product) {
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
+    productCard.dataset.productId = product.id;
 
     productCard.innerHTML = `
         <div class="img-container">
@@ -25,45 +69,23 @@ function createProductCard(product) {
             <span>$${product.price}</span>
         </div>
         <div class="kart-options-container">
-           <button>-</button>
+           <button class="min-btn">-</button>
            <input type="text" value="1" disabled>
-           <button>+</button>
+           <button class="max-btn">+</button>
         </div> 
         <div class="kart-btn-container">
             <button>Agregar al carrito</button>
-        </div>`;
+        </div>`;          
+
+    productCardEventSuscribe(productCard); 
 
     return productCard;
 }
 
-async function getAllProducts(limit, skip) {
 
-    try {
-        const response = await fetch(`https://dummyjson.com/products/category/smartphones?limit=${limit}&skip=${skip}`)
+addEventListener('DOMContentLoaded', () => {
 
-        if (!response.ok)
-            throw new Error(`Error al intentar obtener los productos - Status Code: ${response.status}`);
-
-        const data = await response.json();
-        console.log(data);
-        return data.products.map(x => ToViewModel(x));
-    }
-    catch (error) {
-        console.error(error.message);
-        return null;
-    }
-}
-
-addEventListener('DOMContentLoaded', async () => {
-    const products = await getAllProducts(12, 0);
-
-    const productsGrid = document.querySelector('.grid-products');
-
-    products.forEach(product => {
-        productsGrid.appendChild(createProductCard(product))
-    });
-
-    console.log(products)
+    renderProducts(1);
 });
 
 
